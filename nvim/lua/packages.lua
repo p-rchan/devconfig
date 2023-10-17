fc96d2735fecc9e7613f8994556e2ac67c7e6535
Russ Chan <rc@russchan.com>
2023-10-17 12:45:07 -0400

    changed lualine to tokyonight

--- nvim/lua/packages.lua
+++ nvim/lua/packages.lua
@@ -127,7 +127,7 @@ require('lazy').setup({
     opts = {
       options = {
         icons_enabled = false,
-        theme = 'onedark',
+        theme = 'tokyonight',
         component_separators = '|',
         section_separators = '',
       },
