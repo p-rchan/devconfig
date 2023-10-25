d9f347f039dea985090a8572354f277f4050b3b2
Russ Chan <rc@russchan.com>
2023-10-24 21:17:11 -0400

    put relative path on lualine

--- nvim/lua/packages.lua
+++ nvim/lua/packages.lua
@@ -131,6 +131,9 @@ require('lazy').setup({
         component_separators = '|',
         section_separators = '',
       },
+      sections = {
+        lualine_c = { { 'filename', path = 1} }
+      }
     },
   },
 
