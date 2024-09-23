b0344f028500ee7d1b4c89d4ebcf58718a7b1481
Russ Chan <russ@geta-head.com>
2024-09-23 11:39:18 -0400

    added highlight for active pane

--- nvim/lua/plugins/themes.lua
+++ nvim/lua/plugins/themes.lua
@@ -1,23 +1,31 @@
 return {
-  {
-    -- Theme inspired by Atom
-    'navarasu/onedark.nvim',
-    priority = 1000,
-    config = function()
-      vim.g.onedark_config = { style = 'deep' }
-      -- vim.cmd( [[ colorscheme onedark ]] )
-    end,
-  },
-  {
-    -- Theme inspired by Atom
-    'folke/tokyonight.nvim',
-    lazy = false, --load during startup
-    priority = 1000, --load before all other start plugins
-    config = function()
-      -- load the colorscheme
-      vim.cmd([[colorscheme tokyonight]])
-    end
-  },
-
-
+	{
+		-- Theme inspired by Atom
+		"navarasu/onedark.nvim",
+		priority = 1000,
+		opts = {
+			style = "deep",
+		},
+		config = function()
+			--vim.g.onedark_config = { style = "deep" }
+			--vim.cmd([[ colorscheme onedark ]])
+		end,
+	},
+	{
+		-- Theme inspired by Atom
+		"folke/tokyonight.nvim",
+		lazy = false, --load during startup
+		priority = 1000, --load before all other start plugins
+		enabled = true,
+		opts = {},
+		config = function()
+			-- load the colorscheme
+			vim.cmd([[colorscheme tokyonight]])
+		end,
+	},
+	{
+		"nvim-zh/colorful-winsep.nvim",
+		config = true,
+		event = { "WinLeave" },
+	},
 }
